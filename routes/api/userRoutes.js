9defb0977c6faa9bed9593cd4fef2fd64c49cfe0
EChrist01 <ethanchrist02@gmail.com>
2024-02-19 15:20:57 -0700

    models-api/routes-package install

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+} = require("../../controllers/userController");
+
+// http://localhost:3001/api/users
+router.route("/").get(getUsers).post(createUser);
+
+// http://localhost:3001/api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// http://localhost:3001/api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend);
+
+module.exports = router;
